06095f8018f255902b39b5ef87f066ebc70fc486
NBenAbdellah <n.ben-abdellah@outlook.com>
2026-03-18 14:42:07 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ module.exports = function() {
 
   /* istanbul ignore next */
   app.get('/hi', (req, res) => {
-    res.status(200).send('Hi there!');
+    res.status(200).send('Hi there Nasser! Welcome to your completed CI/CD pipeline! :)');
   });
 
   return app;
